8e9f1b9cd99d4b4d63ff3a49fb75e8e5fc4f5ca5
Debbie Wright <debbiewright@live.co.uk>
2022-09-06 18:10:46 +0100

    CSS added to form for adding book

--- script.js
+++ script.js
@@ -69,3 +69,25 @@ function getLibrary(){
 }
 
 getLibrary();
+
+/*-------- Modal -------*/
+
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("add-book-btn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
