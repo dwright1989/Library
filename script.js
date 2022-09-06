0c60a755e069ccc49a359b6c7180e284105b8bcd
Debbie Wright <debbiewright@live.co.uk>
2022-09-06 16:49:15 +0100

    Style added with script to show library

--- script.js
+++ script.js
@@ -18,6 +18,10 @@ class Book{
 /* To be removed (manually added for now) */
 let book1 = new Book("Harry Potter", "J.K. Rowling", 123, true);
 let book2 = new Book("The Hobbit", "J.R.R. Tolkien", 456, false);
+let book3 = new Book("Twilight", "Stephanie Meyer", 400, false);
+let book4 = new Book("Blind Faith", "Ben elton", 543, false);
+let book5 = new Book("The Handmaid's Tale", "Margaret Atwood", 399, false);
+let book6 = new Book("Misery", "Stephen King", 619, true);
 
 
 function addBookToLibrary(book) {
@@ -26,11 +30,40 @@ function addBookToLibrary(book) {
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
+addBookToLibrary(book3);
+addBookToLibrary(book4);
+addBookToLibrary(book5);
+addBookToLibrary(book6);
 
 
 function getLibrary(){
     myLibrary.forEach(function (item, index) {
-    console.log(item, index);
+        console.log(item, index);
+        let booksDiv = document.getElementById("books");
+        let bookDiv = document.createElement("div");
+        bookDiv.classList.add("book");
+
+        let bookTitle = document.createElement("h2");
+        bookTitle.innerHTML  = item.title;
+
+        let bookAuthor = document.createElement("h4");
+        bookAuthor.innerHTML  = "Author: " + item.author;
+
+        let bookPages = document.createElement("p");
+        bookPages.innerHTML  = "Pages: " + item.pages;
+
+        let haveRead = document.createElement("p");
+        if(item.read){
+            haveRead.innerHTML  = "read";
+        }else{
+            haveRead.innerHTML  = "not read";
+        }
+        
+        bookDiv.appendChild(bookTitle);
+        bookDiv.appendChild(bookAuthor);
+        bookDiv.appendChild(bookPages);
+        bookDiv.appendChild(haveRead);
+        booksDiv.appendChild(bookDiv);
     });
 }
 
